2efcf63eba4bd2ba2f420022f813eade9143f1ae
uQlel <jchajdys@gmail.com>
2021-08-02 19:50:25 +0200

    Moved events to handlers

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 const fs = require("fs")
 require("dotenv").config()
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const handlerFiles = fs.readdirSync('./handlers').filter(file => file.endsWith('.js'));
 
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args,client));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args,client));
-    }
+for (const file of handlerFiles) {
+    const handler = require(`./handlers/${file}`)
+    handler.execute(client)
 }
 
 client.login(process.env.TOKEN)
